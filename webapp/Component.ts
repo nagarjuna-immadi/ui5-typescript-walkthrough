aa9fe64eedfb404b3b0792976acd24715b65ffdb
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2025-04-09 21:09:11 +0200

    descriptor

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -2,14 +2,14 @@ import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import JSONModel from "sap/ui/model/json/JSONModel";
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
 * @namespace ui5.walkthrough
 */
 export default class Component extends UIComponent {
     public static metadata = {
-        "interfaces": ["sap.ui.core.IAsyncContentCreation"]
+        "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+        "manifest": "json"
     };
 
     init(): void {
@@ -24,18 +24,5 @@ export default class Component extends UIComponent {
         };
         const dataModel = new JSONModel(data);
         this.setModel(dataModel);
-
-        // set i18n model
-        const i18nModel = new ResourceModel({
-            bundleName: "ui5.walkthrough.i18n.i18n"
-        });
-        this.setModel(i18nModel, "i18n");
-    };
-
-    createContent(): Control | Promise<Control | null> | null {
-        return XMLView.create({
-            "viewName": "ui5.walkthrough.view.App",
-            "id": "app"
-        });
     };
 };
