e7c64cd0b2f7533ddbafa7074dea8c9ce010b556
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2025-04-11 15:27:00 +0200

    integration tests setup

--- webapp/test/testsuite.qunit.ts
+++ webapp/test/testsuite.qunit.ts
@@ -16,7 +16,10 @@ export default {
     },
     tests: {
            "unit/unitTests": {
-                   title: "UI5 TypeScript Walkthrough - Unit Tests"
+                title: "UI5 TypeScript Walkthrough - Unit Tests"
+           },
+           "integration/opaTests": {
+                title: "UI5 TypeScript Walkthrough - Integration Tests"
            }
     }
 };
