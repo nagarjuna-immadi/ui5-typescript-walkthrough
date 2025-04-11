e54ed2db7ccbf71a03c8d5f2ffdc80ec0538dc03
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2025-04-11 16:33:25 +0200

    product rating custom control

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -2,6 +2,10 @@ import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import History from "sap/ui/core/routing/History";
+import MessageToast from "sap/m/MessageToast";
+import ProductRating, { ProductRating$ChangeEvent } from "../control/ProductRating";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @namespace ui5.walkthrough.controller
@@ -14,6 +18,7 @@ export default class Detail extends Controller {
     }
 
     onObjectMatched(event: Route$PatternMatchedEvent): void {
+        (<ProductRating> this.byId("rating")).reset();
         this.getView().bindElement({
             path: "/" + window.decodeURIComponent( (event.getParameter("arguments") as any).invoicePath),
             model: "invoice"
@@ -30,5 +35,12 @@ export default class Detail extends Controller {
             const router = UIComponent.getRouterFor(this);
             router.navTo("overview", {}, true);
         }
+    }
+
+    onRatingChange(event: ProductRating$ChangeEvent): void {
+        const value = event.getParameter("value");
+        const resourceBundle = <ResourceBundle> (<ResourceModel> this?.getView().getModel("i18n"))?.getResourceBundle();
+		
+        MessageToast.show(resourceBundle.getText("ratingConfirmation", [value]));
     }
 };
